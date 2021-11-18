00459c4ae08a0cdeb6dec870b91cae61291e607f
anna bozio <anna.bozio@gmail.com>
2021-11-18 13:34:14 +0100

    add index and show to plants controller and view

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
   belongs_to :user
+  validates :name, uniqueness: { message: `%{value} is already taken` }
+  validates :rythme, presence: { message: "veuillez indiquer à quelle frequence arroser cette plante" }
 end
