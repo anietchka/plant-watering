b08bfb6f87cc759425c5d799424d9bbb5ff8bb88
anna bozio <anna.bozio@gmail.com>
2021-11-18 13:57:42 +0100

    add new and create to plants controller and view

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,4 +6,20 @@ class PlantsController < ApplicationController
   def show
     @plant = Plant.find(params[:id])
   end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.save
+    redirect_to plant_path(@plant)
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :rytme, :photo)
+  end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,5 @@
 class Plant < ApplicationRecord
   belongs_to :user
   validates :name, uniqueness: { message: `%{value} is already taken` }
-  validates :rythme, presence: { message: "veuillez indiquer à quelle frequence arroser cette plante" }
+  validates :rytme, presence: { message: "veuillez indiquer à quelle frequence arroser cette plante" }
 end
