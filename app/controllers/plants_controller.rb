aa253814a96e756f0f6855d567dd6c7310b8e2ac
anna bozio <anna.bozio@gmail.com>
2021-11-18 17:57:01 +0100

    add card plant to index

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,12 @@ class PlantsController < ApplicationController
     redirect_to plant_path(@plant)
   end
 
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    redirect_to plants_path
+  end
+
   private
 
   def plant_params
