0c6ca832a2e84e43c119cc27d7261df7ccc7571f
anna bozio <anna.bozio@gmail.com>
2021-11-24 15:48:45 +0100

    add dashboard

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,16 +1,13 @@
 class PlantsController < ApplicationController
   def index
     @plants = Plant.all
-    @user = current_user
   end
 
   def show
-    @user = current_user
     @plant = Plant.find(params[:id])
   end
 
   def new
-    @user = current_user
     @plant = Plant.new
   end
 
@@ -22,7 +19,6 @@ class PlantsController < ApplicationController
   end
 
   def destroy
-    @user = current_user
     @plant = Plant.find(params[:id])
     @plant.destroy
     redirect_to plants_path
