f707ab9c3beeb64efe199961f2e6768fde7c8b02
anna bozio <anna.bozio@gmail.com>
2021-12-13 16:22:01 +0100

    add watered and plants to water to controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,10 +1,10 @@
 class PlantsController < ApplicationController
+  before_action :find_plant, only: [:show, :destroy, :watered]
   def index
     @plants = Plant.where(user: current_user)
   end
 
   def show
-    @plant = Plant.find(params[:id])
   end
 
   def new
@@ -19,14 +19,26 @@ class PlantsController < ApplicationController
   end
 
   def destroy
-    @plant = Plant.find(params[:id])
     @plant.destroy
     redirect_to plants_path
+  end
+
+  def watered
+    @plant.update!(next_watered_at: @plant.rythm.days.from_now)
+    redirect_to plant_path(@plant)
+  end
+
+  def plants_to_water
+    @plants = Plant.where(user: current_user, alive: true).where('next_watered_at <= ?', Date.today)
   end
 
   private
 
   def plant_params
     params.require(:plant).permit(:name, :rythm, :photo)
+  end
+
+  def find_plant
+    @plant = Plant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :plants
+  resources :plants do
+    member do
+      patch 'watered'
+    end
+    collection do
+      get 'plants_to_water'
+    end
+  end
 end
